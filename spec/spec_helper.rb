6b03bf004eabf8be87f20bd7912662c2560f37c9
Pete Yandell <pete@notahat.com>
2008-11-08 10:54:48 +1100

    Fixed up broken autotest depencies.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'test/unit'
 require 'spec'
+require 'sham'
 
 Spec::Runner.configure do |config|
   config.before(:each) { Sham.reset }
