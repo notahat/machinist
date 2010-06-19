45fddf23a52b2f9b28a396b849008da286d1b2eb
Pete Yandell <pete@notahat.com>
2010-06-20 01:48:26 +1000

    Cleaned up badly named methods in Machinable.

--- lib/machinist/machinable.rb
+++ lib/machinist/machinable.rb
@@ -1,6 +1,5 @@
 module Machinist
   module Machinable
-
     def blueprint(name = :master, &block)
       @blueprints ||= {}
       parent = @blueprints[:master] unless name == :master
@@ -8,13 +7,13 @@ module Machinist
     end
 
     def make(*args)
-      process(*args) do |blueprint, attributes|
+      decode_args_to_make(*args) do |blueprint, attributes|
         blueprint.make(attributes)
       end
     end
 
     def make!(*args)
-      process(*args) do |blueprint, attributes|
+      decode_args_to_make(*args) do |blueprint, attributes|
         Shop.buy(blueprint, attributes)
       end
     end
@@ -29,12 +28,11 @@ module Machinist
 
   private
 
-    # FIXME: Needs a better name. This stuff is polluting the namespace of the
-    # object that includes it.
-    def process(*args)
-      count      = shift_arg(args, Fixnum)
-      name       = shift_arg(args, Symbol) || :master
-      attributes = shift_arg(args, Hash)   || {}
+    def decode_args_to_make(*args)
+      shift_arg = lambda {|klass| args.shift if args.first.is_a?(klass) }
+      count      = shift_arg[Fixnum]
+      name       = shift_arg[Symbol] || :master
+      attributes = shift_arg[Hash]   || {}
       raise ArgumentError unless args.empty?  # FIXME: Meaningful exception.
 
       @blueprints ||= {}
@@ -46,12 +44,6 @@ module Machinist
       else
         Array.new(count) { yield(blueprint, attributes) }
       end
-    end
-
-    # FIXME: Needs a better name. This stuff is polluting the namespace of the
-    # object that includes it.
-    def shift_arg(args, klass)
-      args.shift if args.first.is_a?(klass)
     end
 
   end
