b70dceb3e495303370d15278398b1698a8fae360
Pete Yandell <pete@notahat.com>
2010-06-23 00:33:55 +1000

    Docs for warehouse.

--- lib/machinist/warehouse.rb
+++ lib/machinist/warehouse.rb
@@ -1,5 +1,23 @@
 module Machinist
-  class Warehouse < Hash #:nodoc:
+
+  # A Warehouse is just a hash that makes it easy to key off arrays of objects.
+  #
+  # For example, if you assign:
+  #
+  #     warehouse[1, 2] = "Hello, world!"
+  #
+  # then:
+  #
+  #     warehouse.has_key?([1, 2])
+  #
+  # will be true, and:
+  #
+  #     warehouse[1, 2]
+  #
+  # will return "Hello, world!"
+  class Warehouse < Hash
+
+    # Return a new warehouse with the same keys, and dups of all the values.
     def clone
       clone = Warehouse.new
       each_pair do |key, value|
@@ -8,14 +26,17 @@ module Machinist
       clone
     end
 
+    # Assign a value for the given list of keys.
     def []=(*keys)
       value = keys.pop
       super(keys, value)
     end
 
+    # Return the value for the given list of keys.
     def [](*keys)
       self[*keys] = [] if !has_key?(keys)
       super(keys)
     end
+
   end
 end
