fbbe7bc88387c742a2bc61e8e50f888cff1f0350
Pete Yandell <pete@notahat.com>
2010-06-23 11:22:25 +1000

    Fixed requires in machinist.rb.

--- lib/machinist.rb
+++ lib/machinist.rb
@@ -1,8 +1,8 @@
-require 'machinist/blueprint.rb'
-require 'machinist/lathe.rb'
-require 'machinist/machinable.rb'
-require 'machinist/shop.rb'
-require 'machinist/warehouse.rb'
+require 'machinist/blueprint'
+require 'machinist/lathe'
+require 'machinist/machinable'
+require 'machinist/shop'
+require 'machinist/warehouse'
 
 module Machinist
 
