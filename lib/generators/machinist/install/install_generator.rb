de3031aa1fa1602068bb09d8ed93b6e7ecf7e2ad
Pete Yandell <pete@notahat.com>
2010-06-26 18:18:56 +1000

    Rails 3 generators for Test::Unit and Cucumber.
    I haven't tested this well. YMMV.

--- lib/generators/machinist/install/install_generator.rb
+++ lib/generators/machinist/install/install_generator.rb
@@ -3,18 +3,52 @@ module Machinist
     class InstallGenerator < Rails::Generators::Base #:nodoc:
       source_root File.expand_path('../templates', __FILE__)
 
-      desc <<DESC
-Description:
-    Copy Machinist files to your application.
-DESC
+      class_option :test_framework, :type => :string, :aliases => "-t", :desc => "Test framework to use Machinist with"
+      class_option :cucumber, :type => :boolean, :desc => "Set up access to Machinist from Cucumber"
 
       def generate_blueprints_file
-        copy_file "blueprints.rb", "spec/support/blueprints.rb" 
+        if rspec?
+          copy_file "blueprints.rb", "spec/support/blueprints.rb" 
+        else
+          copy_file "blueprints.rb", "test/blueprints.rb"
+        end
       end
 
       def install_shop_reset
-        text = "  # Reset the Machinist cache before each spec\n  config.before(:each) { Machinist.reset_before_test }\n\n"
-        inject_into_file "spec/spec_helper.rb", text, :after => "Rspec.configure do |config|\n"
+        if rspec?
+          inject_into_file(
+            "spec/spec_helper.rb",
+            "  # Reset the Machinist cache before each spec.\n  config.before(:each) { Machinist.reset_before_test }\n\n",
+            :after => "Rspec.configure do |config|\n"
+          )
+        else
+          inject_into_file(
+            "test/test_helper.rb",
+            "require File.expand_path(File.dirname(__FILE__) + '/blueprints')\n",
+            :after => "require 'rails/test_help'\n"
+          )
+          inject_into_file(
+            "test/test_helper.rb",
+            "  # Reset the Machinist cache before each test.\n  setup { Machinist.reset_before_test }\n\n",
+            :after => "class ActiveSupport::TestCase\n"
+          )
+        end
+      end
+      
+      def install_cucumber_support
+        if cucumber?
+          template "machinist.rb.erb", "features/support/machinist.rb"
+        end
+      end
+
+    private
+
+      def rspec?
+        options[:test_framework].to_sym == :rspec
+      end
+
+      def cucumber?
+        options[:cucumber]
       end
 
     end
