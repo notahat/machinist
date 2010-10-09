04a734548903438bad1fa793806919ad216ab074
Ches Martin <ches@whiskeyandgrits.net>
2010-10-09 17:05:23 -0400

    Create field stubs in the Rails model generator.

--- lib/generators/machinist/model/model_generator.rb
+++ lib/generators/machinist/model/model_generator.rb
@@ -4,7 +4,16 @@ module Machinist
       argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
       def create_blueprint
-        append_file "spec/support/blueprints.rb", "\n#{class_name}.blueprint do\n  # Attributes here\nend\n"
+        if attributes.empty?
+          fields = "  # Attributes here\n"
+        else
+          fields = ''
+          attributes.each do |a|
+            fields << "  #{a.name} { #{a.default.inspect} }\n"
+          end
+        end
+
+        append_file "spec/support/blueprints.rb", "\n#{class_name}.blueprint do\n#{fields}end\n"
       end
 
     end
