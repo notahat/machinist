0919a86fda193198ea9ae8d869a5b952ffeb81a1
Chris Corbyn <chris@w3style.co.uk>
2011-06-05 15:59:39 +1000

    Better integration with DM (fixes issue where DataMapper::Resource was already included before Machinist)

--- lib/machinist/data_mapper.rb
+++ lib/machinist/data_mapper.rb
@@ -3,20 +3,12 @@ require 'machinist'
 require 'machinist/data_mapper/blueprint'
 require 'machinist/data_mapper/lathe'
 
-module DataMapper::Resource #:nodoc:
-  # FIXME: Maybe there's a better way to override the mixin post-include logic?
-  class << self
-    alias_method :old_included, :included
-    
-    def included(resource)
-      old_included(resource)
-      resource.extend Machinist::Machinable
-      
-      class << resource
-        def blueprint_class
-          Machinist::DataMapper::Blueprint
-        end
-      end
-    end
-  end
+module Machinist::DataMapper
+	module BlueprintExtension
+	  def blueprint_class
+	    Machinist::DataMapper::Blueprint
+	  end
+	end 
 end
+
+DataMapper::Model.append_extensions(Machinist::Machinable, Machinist::DataMapper::BlueprintExtension)
