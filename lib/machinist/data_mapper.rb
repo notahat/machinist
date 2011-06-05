f4bbb1fe46b1f80a823f46f08f1f8b579a2f8f76
Chris Corbyn <chris@w3style.co.uk>
2011-06-05 17:45:20 +1000

    Grr, damn editor. Remove tab indentation.

--- lib/machinist/data_mapper.rb
+++ lib/machinist/data_mapper.rb
@@ -4,11 +4,11 @@ require 'machinist/data_mapper/blueprint'
 require 'machinist/data_mapper/lathe'
 
 module Machinist::DataMapper
-	module BlueprintExtension
-	  def blueprint_class
+  module BlueprintExtension
+    def blueprint_class
 	    Machinist::DataMapper::Blueprint
-	  end
-	end 
+    end
+  end 
 end
 
 DataMapper::Model.append_extensions(Machinist::Machinable, Machinist::DataMapper::BlueprintExtension)
